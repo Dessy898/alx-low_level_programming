e0dc9434e222a4f1b98db1de66c57a84cef0a2f2
Dessy898 <dessy898@gmail.com>
2023-06-15 16:49:51 -0700

    100-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include <unistd.h>
 /**
-* main - Prints a text
-* Return: Always 1 (Success)
+* main - Entry point
+* Return: Always 1 (Error code)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar,
-2015-10-19\n", 59);
-return (1);
-}
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
