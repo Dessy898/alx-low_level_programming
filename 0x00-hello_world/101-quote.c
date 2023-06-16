2becf84932c2605d52d7e770883157a191975347
Dessy898 <dessy898@gmail.com>
2023-06-15 18:00:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
-* main - Entry point
-* Return: Always 1 (Error code)
-*/
+ * main - A c program that print a line to a standard error
+ * Return: 1 (successful)
+ */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
